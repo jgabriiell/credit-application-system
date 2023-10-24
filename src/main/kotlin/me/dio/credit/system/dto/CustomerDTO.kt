07dd7b390af64c3e4312324b056df6e2c268047f
João Gabriel <jgabriiell33@gmail.com>
2023-10-24 20:40:10 -0300

    feat: create dto

---
+++ src/main/kotlin/me/dio/credit/system/dto/CustomerDTO.kt
@@ -0,0 +1,29 @@
+package me.dio.credit.system.dto
+
+import me.dio.credit.system.domain.Address
+import me.dio.credit.system.domain.Customer
+import java.math.BigDecimal
+
+data class CustomerDTO(
+    val firstName: String,
+    val lastName: String,
+    val cpf: String,
+    val income: BigDecimal,
+    val email: String,
+    val password: String,
+    val zipCode: String,
+    val street: String
+) {
+    fun toEntity(): Customer = Customer (
+        firstName = this.firstName,
+        lastName = this.lastName,
+        cpf = this.cpf,
+        income = this.income,
+        email = this.email,
+        password = this.password,
+        address = Address(
+            zipCode = this.zipCode,
+            street = this.street
+        )
+    )
+}
